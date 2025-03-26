8d1078e3e72bddae2515ec96631a9e0461e44df5
merlotje <margot.verheij@coolblue.nl>
2025-03-26 16:26:20 +0100

    [GIRLSCOUT] Omit enddate from entso data

--- dbt_workspace/models/intermediate/totalload_dayahead_2022_2025.sql
+++ dbt_workspace/models/intermediate/totalload_dayahead_2022_2025.sql
@@ -11,9 +11,7 @@
 
 SELECT
   REGEXP_EXTRACT(start_datetime, r'(\d{2}-\d{2}-\d{4})') AS start_date
-  , REGEXP_EXTRACT(end_datetime, r'(\d{2}-\d{2}-\d{4})') AS end_date
   , CAST(REGEXP_EXTRACT(SPLIT(start_datetime, ' ')[OFFSET(1)], r'^(\d{2})') AS INT64) AS start_hour
-  , CAST(REGEXP_EXTRACT(SPLIT(end_datetime, ' ')[OFFSET(1)], r'^(\d{2})') AS INT64) AS end_hour
   , day_ahead_total_load_forecast
   , actual_total_load
 FROM {{ ref('totalload_dayahead_actual_2022') }}
@@ -22,9 +20,7 @@ UNION ALL
 
 SELECT
   REGEXP_EXTRACT(start_datetime, r'(\d{2}-\d{2}-\d{4})') AS start_date
-  , REGEXP_EXTRACT(end_datetime, r'(\d{2}-\d{2}-\d{4})') AS end_date
   , CAST(REGEXP_EXTRACT(SPLIT(start_datetime, ' ')[OFFSET(1)], r'^(\d{2})') AS INT64) AS start_hour
-  , CAST(REGEXP_EXTRACT(SPLIT(end_datetime, ' ')[OFFSET(1)], r'^(\d{2})') AS INT64) AS end_hour
   , day_ahead_total_load_forecast
   , actual_total_load
 FROM {{ ref('totalload_dayahead_actual_2023') }}
@@ -33,9 +29,7 @@ UNION ALL
 
 SELECT
   REGEXP_EXTRACT(start_datetime, r'(\d{2}-\d{2}-\d{4})') AS start_date
-  , REGEXP_EXTRACT(end_datetime, r'(\d{2}-\d{2}-\d{4})') AS end_date
   , CAST(REGEXP_EXTRACT(SPLIT(start_datetime, ' ')[OFFSET(1)], r'^(\d{2})') AS INT64) AS start_hour
-  , CAST(REGEXP_EXTRACT(SPLIT(end_datetime, ' ')[OFFSET(1)], r'^(\d{2})') AS INT64) AS end_hour
   , day_ahead_total_load_forecast
   , actual_total_load
 FROM {{ ref('totalload_dayahead_actual_2024') }}
@@ -44,9 +38,7 @@ UNION ALL
 
 SELECT
   REGEXP_EXTRACT(start_datetime, r'(\d{2}-\d{2}-\d{4})') AS start_date
-  , REGEXP_EXTRACT(end_datetime, r'(\d{2}-\d{2}-\d{4})') AS end_date
   , CAST(REGEXP_EXTRACT(SPLIT(start_datetime, ' ')[OFFSET(1)], r'^(\d{2})') AS INT64) AS start_hour
-  , CAST(REGEXP_EXTRACT(SPLIT(end_datetime, ' ')[OFFSET(1)], r'^(\d{2})') AS INT64) AS end_hour
   , day_ahead_total_load_forecast
   , actual_total_load
 FROM {{ ref('totalload_dayahead_actual_2025') }}
